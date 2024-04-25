6eee43167ef2a2afea5277eb3348f714dcce553f
surajring7024 <suraj.ingole7024@gmail.com>
2024-04-25 18:49:07 +0530

    Day 2 Commit. Used props started building swiggy app

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import ReactDOM from "react-dom/client";
 
 const Header = () => {
   return (
@@ -19,26 +19,324 @@ const Header = () => {
   );
 };
 
-const RestaurantCard = () => {
-  return (
-    <div className="res-card">
-      <img
-        src={require("./Resources/images/food.jpg")}
-        className="res-card-img"
-      ></img>
-      <div className="">
-        <h3>Meghana Foods</h3>
-      </div>
-    </div>
-  );
-};
+const data = [
+  {
+    info: {
+      id: "558083",
+      name: "Geetham Veg restaurant",
+      cloudinaryImageId: "t8o1vgx93ztwz8qcmg7r",
+      locality: "Rajiv Gandhi Salai Navalur",
+      areaName: "OMR Navalur",
+      costForTwo: "₹250 for two",
+      cuisines: ["South Indian", "Sweets"],
+      avgRating: 4.5,
+      veg: true,
+      parentId: "335557",
+      avgRatingString: "4.5",
+      totalRatingsString: "10K+",
+      promoted: true,
+      adTrackingId:
+        "cid=12903293~p=0~adgrpid=12903293#ag1~mp=SWIGGY_IN~bl=FOOD~aet=RESTAURANT~aeid=558083~eid=18ecdb06-8fa1-40d9-b57c-4512f8fcbda1~srvts=1713945467570~collid=83646",
+      sla: {
+        deliveryTime: 42,
+        lastMileTravel: 6.2,
+        serviceability: "SERVICEABLE",
+        slaString: "40-45 mins",
+        lastMileTravelString: "6.2 km",
+        iconType: "ICON_TYPE_EMPTY",
+      },
+    },
+  },
+  {
+    info: {
+      id: "690702",
+      name: "Pizza Hut",
+      cloudinaryImageId: "2b4f62d606d1b2bfba9ba9e5386fabb7",
+      locality: "Karapakkam",
+      areaName: "Thoraipakkam",
+      costForTwo: "₹350 for two",
+      cuisines: ["Pizzas"],
+      avgRating: 4.2,
+      parentId: "721",
+      avgRatingString: "4.2",
+      totalRatingsString: "1K+",
+      sla: {
+        deliveryTime: 35,
+        lastMileTravel: 6.4,
+        serviceability: "SERVICEABLE",
+        slaString: "30-35 mins",
+        lastMileTravelString: "6.4 km",
+        iconType: "ICON_TYPE_EMPTY",
+      },
+    },
+  },
+  {
+    info: {
+      id: "690703",
+      name: "Pizza Hut",
+      cloudinaryImageId: "2b4f62d606d1b2bfba9ba9e5386fabb7",
+      locality: "Karapakkam",
+      areaName: "Thoraipakkam",
+      costForTwo: "₹350 for two",
+      cuisines: ["Pizzas"],
+      avgRating: 4.2,
+      parentId: "721",
+      avgRatingString: "4.2",
+      totalRatingsString: "1K+",
+      sla: {
+        deliveryTime: 35,
+        lastMileTravel: 6.4,
+        serviceability: "SERVICEABLE",
+        slaString: "30-35 mins",
+        lastMileTravelString: "6.4 km",
+        iconType: "ICON_TYPE_EMPTY",
+      },
+    },
+  },
+  {
+    info: {
+      id: "690704",
+      name: "Pizza Hut",
+      cloudinaryImageId: "2b4f62d606d1b2bfba9ba9e5386fabb7",
+      locality: "Karapakkam",
+      areaName: "Thoraipakkam",
+      costForTwo: "₹350 for two",
+      cuisines: ["Pizzas"],
+      avgRating: 4.2,
+      parentId: "721",
+      avgRatingString: "4.2",
+      totalRatingsString: "1K+",
+      sla: {
+        deliveryTime: 35,
+        lastMileTravel: 6.4,
+        serviceability: "SERVICEABLE",
+        slaString: "30-35 mins",
+        lastMileTravelString: "6.4 km",
+        iconType: "ICON_TYPE_EMPTY",
+      },
+    },
+  },
+  {
+    info: {
+      id: "690705",
+      name: "Pizza Hut",
+      cloudinaryImageId: "2b4f62d606d1b2bfba9ba9e5386fabb7",
+      locality: "Karapakkam",
+      areaName: "Thoraipakkam",
+      costForTwo: "₹350 for two",
+      cuisines: ["Pizzas"],
+      avgRating: 4.2,
+      parentId: "721",
+      avgRatingString: "4.2",
+      totalRatingsString: "1K+",
+      sla: {
+        deliveryTime: 35,
+        lastMileTravel: 6.4,
+        serviceability: "SERVICEABLE",
+        slaString: "30-35 mins",
+        lastMileTravelString: "6.4 km",
+        iconType: "ICON_TYPE_EMPTY",
+      },
+    },
+  },
+  {
+    info: {
+      id: "690706",
+      name: "Pizza Hut",
+      cloudinaryImageId: "2b4f62d606d1b2bfba9ba9e5386fabb7",
+      locality: "Karapakkam",
+      areaName: "Thoraipakkam",
+      costForTwo: "₹350 for two",
+      cuisines: ["Pizzas"],
+      avgRating: 4.2,
+      parentId: "721",
+      avgRatingString: "4.2",
+      totalRatingsString: "1K+",
+      sla: {
+        deliveryTime: 35,
+        lastMileTravel: 6.4,
+        serviceability: "SERVICEABLE",
+        slaString: "30-35 mins",
+        lastMileTravelString: "6.4 km",
+        iconType: "ICON_TYPE_EMPTY",
+      },
+    },
+  },
+  {
+    info: {
+      id: "690707",
+      name: "Pizza Hut",
+      cloudinaryImageId: "2b4f62d606d1b2bfba9ba9e5386fabb7",
+      locality: "Karapakkam",
+      areaName: "Thoraipakkam",
+      costForTwo: "₹350 for two",
+      cuisines: ["Pizzas"],
+      avgRating: 4.2,
+      parentId: "721",
+      avgRatingString: "4.2",
+      totalRatingsString: "1K+",
+      sla: {
+        deliveryTime: 35,
+        lastMileTravel: 6.4,
+        serviceability: "SERVICEABLE",
+        slaString: "30-35 mins",
+        lastMileTravelString: "6.4 km",
+        iconType: "ICON_TYPE_EMPTY",
+      },
+    },
+  },
+  {
+    info: {
+      id: "690708",
+      name: "Pizza Hut",
+      cloudinaryImageId: "2b4f62d606d1b2bfba9ba9e5386fabb7",
+      locality: "Karapakkam",
+      areaName: "Thoraipakkam",
+      costForTwo: "₹350 for two",
+      cuisines: ["Pizzas"],
+      avgRating: 4.2,
+      parentId: "721",
+      avgRatingString: "4.2",
+      totalRatingsString: "1K+",
+      sla: {
+        deliveryTime: 35,
+        lastMileTravel: 6.4,
+        serviceability: "SERVICEABLE",
+        slaString: "30-35 mins",
+        lastMileTravelString: "6.4 km",
+        iconType: "ICON_TYPE_EMPTY",
+      },
+    },
+  },
+  {
+    info: {
+      id: "690709",
+      name: "Pizza Hut",
+      cloudinaryImageId: "2b4f62d606d1b2bfba9ba9e5386fabb7",
+      locality: "Karapakkam",
+      areaName: "Thoraipakkam",
+      costForTwo: "₹350 for two",
+      cuisines: ["Pizzas"],
+      avgRating: 4.2,
+      parentId: "721",
+      avgRatingString: "4.2",
+      totalRatingsString: "1K+",
+      sla: {
+        deliveryTime: 35,
+        lastMileTravel: 6.4,
+        serviceability: "SERVICEABLE",
+        slaString: "30-35 mins",
+        lastMileTravelString: "6.4 km",
+        iconType: "ICON_TYPE_EMPTY",
+      },
+    },
+  },
+  {
+    info: {
+      id: "690710",
+      name: "Pizza Hut",
+      cloudinaryImageId: "2b4f62d606d1b2bfba9ba9e5386fabb7",
+      locality: "Karapakkam",
+      areaName: "Thoraipakkam",
+      costForTwo: "₹350 for two",
+      cuisines: ["Pizzas"],
+      avgRating: 4.2,
+      parentId: "721",
+      avgRatingString: "4.2",
+      totalRatingsString: "1K+",
+      sla: {
+        deliveryTime: 35,
+        lastMileTravel: 6.4,
+        serviceability: "SERVICEABLE",
+        slaString: "30-35 mins",
+        lastMileTravelString: "6.4 km",
+        iconType: "ICON_TYPE_EMPTY",
+      },
+    },
+  },
+  {
+    info: {
+      id: "690711",
+      name: "Pizza Hut",
+      cloudinaryImageId: "2b4f62d606d1b2bfba9ba9e5386fabb7",
+      locality: "Karapakkam",
+      areaName: "Thoraipakkam",
+      costForTwo: "₹350 for two",
+      cuisines: ["Pizzas"],
+      avgRating: 4.2,
+      parentId: "721",
+      avgRatingString: "4.2",
+      totalRatingsString: "1K+",
+      sla: {
+        deliveryTime: 35,
+        lastMileTravel: 6.4,
+        serviceability: "SERVICEABLE",
+        slaString: "30-35 mins",
+        lastMileTravelString: "6.4 km",
+        iconType: "ICON_TYPE_EMPTY",
+      },
+    },
+  },
+  {
+    info: {
+      id: "690712",
+      name: "Pizza Hut",
+      cloudinaryImageId: "2b4f62d606d1b2bfba9ba9e5386fabb7",
+      locality: "Karapakkam",
+      areaName: "Thoraipakkam",
+      costForTwo: "₹350 for two",
+      cuisines: ["Pizzas"],
+      avgRating: 4.2,
+      parentId: "721",
+      avgRatingString: "4.2",
+      totalRatingsString: "1K+",
+      sla: {
+        deliveryTime: 35,
+        lastMileTravel: 6.4,
+        serviceability: "SERVICEABLE",
+        slaString: "30-35 mins",
+        lastMileTravelString: "6.4 km",
+        iconType: "ICON_TYPE_EMPTY",
+      },
+    },
+  },
+];
 
 const Body = () => {
   return (
     <div className="body">
       <div className="search">Search</div>
       <div className="rest-container">
-        <RestaurantCard></RestaurantCard>
+        {data.map((restaurant) => (
+          <RestaurantCard key={restaurant.info.id} resdata={restaurant} />
+        ))}
+      </div>
+    </div>
+  );
+};
+//console.log(data.card.card.info.name);
+//can use like (props), ({resName,cuisine})
+const RestaurantCard = (props) => {
+  const { resdata } = props;
+
+  //use of optional chaining
+  const { name, cuisines, avgRating, sla } = resdata?.info;
+  return (
+    <div className="res-card">
+      <div className="image-container">
+        <img
+          src={
+            "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/" +
+            resdata.info.cloudinaryImageId
+          }
+          className="res-card-img"
+        ></img>
+      </div>
+      <div className="">
+        <h3>{name}</h3>
+        <h4>{cuisines.join()}</h4>
+        <h4>{avgRating}</h4>
+        <h4>{sla.deliveryTime} minutes</h4>
       </div>
     </div>
   );
